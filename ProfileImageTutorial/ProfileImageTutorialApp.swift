a2c39819208e8d09fd99c3cb547fbdf6ecaa3e5e
Kevin Evans <kevinevansfinlengr@gmail.com>
2022-08-21 14:45:12 -0400

    Initial Commit

---
+++ ProfileImageTutorial/ProfileImageTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileImageTutorialApp.swift
+//  ProfileImageTutorial
+//
+//  Created by KEVIN EVANS on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ProfileImageTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
